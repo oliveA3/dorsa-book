c902dd1c7938e2f895febcd687724285545f3afd
olive.A3 <amanda7u7maya@gmail.com>
2026-03-14 00:39:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-"use strict"
-
-const pages = {
-    page1: {
-
-    },
-    page2: {
-
-    },
-    page3: {
-
-    }
-}
+"use strict";
 
 const flipBook = (elBook) => {
-	elBook.style.setProperty("--c", 0);
-	elBook.querySelectorAll(".page").forEach((page, idx) => {
-		page.style.setProperty("--i", idx);
-		page.addEventListener("click", (evt) => {
-			const curr = evt.target.closest(".back") ? idx : idx + 1;
-			elBook.style.setProperty("--c", curr);
-		});
-	});
+    elBook.style.setProperty("--c", 0);
+    elBook.querySelectorAll(".page").forEach((page, idx) => {
+        page.style.setProperty("--i", idx);
+        page.addEventListener("click", (evt) => {
+            const isFront = evt.target.closest(".front") !== null;
+            const isBack = evt.target.closest(".back") !== null;
+
+            if (isFront) {
+                elBook.style.setProperty("--c", idx + 1);
+            } else if (isBack) {
+                elBook.style.setProperty("--c", idx);
+            }
+        });
+    });
 };
 
 document.querySelectorAll(".book").forEach(flipBook);
